6f03478c8de3850d8fd89cba6b8e97e3ce0a62bb
apelsoczi <apelsoczi@archlinux.us>
2016-05-11 22:08:31 -0400

    Another way to set your on click listeners would be to use XML. - add android:onClick attribute to your <Button> tag. - good practice to use the xml attribute "onClick" over an anonymous Java class whenever possible. + with XML attribute id can be omitted. + Anonymous Java class we have to actively search for the element inside of the view (findViewById portion), but with the XML attribute Android does it for us. + Anonymous Java class we have to name of our method "onClick", but with the XML attribute we can add any name we want, which will dramatically help with the readability of our code. + Xml “onClick” attribute has been added by Google during the API level 4 release, which means that it is a bit more modern syntax and modern syntax is almost always better.

--- app/src/main/java/com/pelsoczi/nanodegreeportfolio/ApplicationActivity.java
+++ app/src/main/java/com/pelsoczi/nanodegreeportfolio/ApplicationActivity.java
@@ -6,56 +6,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class ApplicationActivity extends AppCompatActivity implements Button.OnClickListener {
-
-    private Button one;
-    private Button two;
-    private Button three;
-    private Button four;
-    private Button five;
-    private Button six;
+public class ApplicationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
-
-        one = (Button) findViewById(R.id.one);
-        two = (Button) findViewById(R.id.two);
-        three = (Button) findViewById(R.id.three);
-        four = (Button) findViewById(R.id.four);
-        five = (Button) findViewById(R.id.five);
-        six = (Button) findViewById(R.id.six);
-
-        one.setOnClickListener(this);
-        two.setOnClickListener(this);
-        three.setOnClickListener(this);
-        four.setOnClickListener(this);
-        five.setOnClickListener(this);
-        six.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        String msg = "";
-        if (v.equals(one)) {
-            msg = "one";
-        }
-        else if (v.equals(two)) {
-            msg = "two";
-        }
-        else if (v.equals(three)) {
-            msg = "three";
-        }
-        else if (v.equals(four)) {
-            msg = "four";
-        }
-        else if (v.equals(five)) {
-            msg = "five";
-        }
-        else if (v.equals(six)) {
-            msg = "six";
-        }
+    public void showToast(View v) {
+        Button btn = (Button) v;
+        String msg = (String) btn.getText();
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
